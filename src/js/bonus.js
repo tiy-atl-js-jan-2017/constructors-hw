f154435ab4a721b051bee0de93d6004bfe4a115d
Brit Butler <redline6561@gmail.com>
2017-02-20 22:46:25 -0500

    Initial commit. No tests passing.

---
+++ src/js/bonus.js
@@ -0,0 +1,54 @@
+import {it, expect} from "./test_lib";
+
+// Constructors
+// Only add code to *THIS* section!
+
+///
+///
+///
+///
+
+// Do not ADD or MODIFY code below this line :D
+
+it("should be sleepy at first", function () {
+  let marie = new Human("Marie");
+  expect(marie.alertness < 0.1).toBe(true);
+});
+
+it("needs coffee to wake up", function () {
+  let artem = new Human("Artem");
+  expect(artem.hasCoffee).toBe(false);
+  expect(artem.needsCoffee).toBe(true);
+});
+
+it("can drink coffee to become more alive", function () {
+  let kurt = new Human("Kurt");
+  let omf = new Coffee("Orange Mocha Frappucino");
+  expect(omf.full).toBe(true);
+
+  kurt.buy(omf);
+  kurt.drink();
+  expect(kurt.alertness > 0.3 && kurt.alertness < 0.4).toBe(true);
+  expect(omf.full).toBe(false);
+
+  expect(omf.empty).toBe(false);
+});
+
+it("has coffee after buying it", function () {
+  let tyler = new Human("Tyler");
+  let cuban_blend = new Coffee("Fancy Cuban Blend");
+  expect(tyler.hasCoffee).toBe(false);
+  tyler.buy(cuban_blend);
+  expect(tyler.hasCoffee).toBe(true);
+});
+
+it("can drink all the coffee", function () {
+  let cory = new Human("Cory");
+  let tsmf = new Coffee("Triple Shot Mocha Frappucino");
+  cory.buy(tsmf);
+  for(let i = 0; i < 3; i++) {
+    cory.drink();
+  }
+  expect(tsmf.empty).toBe(true);
+  expect(cory.alertness > 0.9).toBe(true);
+});
---
+++ src/js/main.js
@@ -0,0 +1,63 @@
+import {it, expect} from "./test_lib";
+import { Dog } from "./constructors/dog";
+import { Person } from "./constructors/person";
+
+// Uncomment the line below for Hard Mode
+// Only do this *AFTER* the Normal Mode is finished!
+
+// import from "./bonus";
+
+// Do not ADD or MODIFY code below this line :)
+// Write your code in the constructor files! (dog.js and person.js)
+// Dogs
+let sadie = new Dog({
+  color: 'black',
+  hungry: false
+});
+
+let moonshine = new Dog({
+  color: 'blue-red'
+});
+
+let atticus = new Dog();
+
+// Humans
+let mason = new Person();
+
+let laura = new Person({
+  cool: true
+});
+
+// Tests
+it("should make Sadie happy when Mason pets her", function() {
+  expect(sadie.status).toBe('normal');
+  mason.pet(sadie);
+  expect(sadie.status).toBe('happy');
+});
+
+it("should make Sadie black", function(){
+  expect(sadie.color).toBe('black');
+});
+
+it("should be make Moonshine hungry and Sadie not hungry", function() {
+  expect(moonshine.hungry).toBe(true);
+  expect(sadie.hungry).toBe(false);
+});
+
+it("should make Moonshine no longer hungry when you feed him", function() {
+  laura.feed(moonshine);
+  expect(moonshine.hungry).toBe(false);
+});
+
+
+it("should not affect Atticus and Moonshine's owner properties when setting Mason as Sadie's owner ", function() {
+  sadie.owner = mason;
+  expect(moonshine.owner).toBe(undefined);
+  expect(atticus.owner).toBe(undefined);
+});
+
+it("should make Julia cool and Mason not cool", function() {
+  sadie.owner = mason;
+  expect(laura.cool).toBe(true);
+  expect(mason.cool).toBe(false);
+});
